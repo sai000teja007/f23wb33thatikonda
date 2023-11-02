18654755ecce8e5b689bc012cdbb90f3b4032f4a
sai000teja007 <137807543+sai000teja007@users.noreply.github.com>
2023-11-01 19:13:20 -0500

    Commited rows and columns

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var citiesRouter = require('./routes/cities');
+var boardRouter = require('./routes/board');
+
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cities', citiesRouter);
+app.use('/board', boardRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
